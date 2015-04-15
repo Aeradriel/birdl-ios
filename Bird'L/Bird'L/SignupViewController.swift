489355b25152cab8d2b33f897be5cafb7b6c0dae
pierre-olivier maugis <po.maugis@gmail.com>
2015-04-15 20:04:15 +0800

    viewcontrollers signin and signup created

--- Bird'L/Bird'L/SignupViewController.swift
+++ Bird'L/Bird'L/SignupViewController.swift
@@ -6,4 +6,70 @@
 //  Copyright (c) 2015 Birdl. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var passwordField2: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func nextButtonUp(sender: AnyObject) {
+    }
+}
+
+class SignupViewController2: UIViewController {
+    
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    
+    @IBOutlet weak var genderPicker: UISwitch!
+    @IBOutlet weak var birthDatePicker: UIDatePicker!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func nextButtonUp(sender: AnyObject) {
+    }
+}
+
+class SignupViewController3: UIViewController {
+    
+    @IBOutlet weak var countryPicker: UIPickerView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func validButtonUp(sender: AnyObject) {
+    }
+}
