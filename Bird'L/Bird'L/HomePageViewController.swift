1d9f6f24bd79c45a19496d31121164d574d0e473
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-06-16 19:51:24 +0200

    [A]TabBarController + Navigation controllers for each tab [A]HomeViewController file [A]CircleProgressView file [A]Badge view file

---
+++ Bird'L/Bird'L/HomePageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomePageViewController.swift
+//  Bird'L
+//
+//  Created by Thibaut Roche on 16/06/2015.
+//  Copyright (c) 2015 Birdl. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
