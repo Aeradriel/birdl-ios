bb26f3d60c40ff98be68bd86971282a972d5226f
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-06-18 21:34:17 +0200

    [C]Changes on the badge view [C]Set constraints on the badge view

---
+++ Bird'L/Bird'L/Badge.swift
@@ -0,0 +1,17 @@
+//
+//  Badge.swift
+//  Bird'L
+//
+//  Created by Thibaut Roche on 18/06/2015.
+//  Copyright (c) 2015 Birdl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Badge : UIView
+{
+    @IBOutlet weak var progressCircleView: CircleProgressView!
+    @IBOutlet weak var progressionValue: UILabel!
+    
+}
