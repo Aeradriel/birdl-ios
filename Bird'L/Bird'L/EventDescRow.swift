377234d8e7b7f6f685ff828db5bab95006a71ea1
Thibaut Roche <thibaut.roche.perso@gmail.com>
2015-06-30 15:12:30 +0200

    [A]EventDescRow + EventDescTableViewCell

---
+++ Bird'L/Bird'L/EventDescRow.swift
@@ -0,0 +1,19 @@
+//
+//  EventDescRow.swift
+//  Bird'L
+//
+//  Created by Thibaut Roche on 30/06/2015.
+//  Copyright © 2015 Birdl. All rights reserved.
+//
+
+import Foundation
+
+class EventDescRow : EventRow
+{
+    var desc: String
+    
+    init(desc: String)
+    {
+        self.desc = desc
+    }
+}
---
+++ Bird'L/Bird'L/EventDescTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  EventDescTableViewCell.swift
+//  Bird'L
+//
+//  Created by Thibaut Roche on 30/06/2015.
+//  Copyright © 2015 Birdl. All rights reserved.
+//
+
+import UIKit
+
+class EventDescTableViewCell: EventDetailTableViewCell
+{
+    @IBOutlet weak var desc: UITextView!
+    
+    //MARK: UITableViewCell methods
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    //MARK: Custom methods
+    override func fillCell(row: EventRow)
+    {
+        if let titleRow = row as? EventDescRow
+        {
+            self.desc.text = titleRow.desc
+        }
+    }
+}
